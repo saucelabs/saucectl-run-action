12d1d6dd986476b9e52f1f287574cae2575da8b7
Félix P <felix@saucelabs.com>
2021-01-27 13:51:04 -0800

    Correct version fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const core = require("@actions/core");
 const github = require("@actions/github");
 const childProcess = require("child_process");
 const tc = require('@actions/tool-cache');
@@ -8,7 +7,7 @@ const config = require("./config");
 
 async function run() {
     const cfg = config.get();
-    await installSaucelabs(cfg.saucectlVersion);
+    await installSaucelabs({ versionSpec: cfg.saucectlVersion });
 
     const child = childProcess.spawn('saucectl', ['--version']);
     child.stdout.pipe(process.stdout);
