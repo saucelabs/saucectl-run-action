cbb0208fd70b61d474beddd5c5a77ea7551db528
Félix P <felix@saucelabs.com>
2021-01-27 13:39:03 -0800

    Add missing config

---
+++ src/config.js
@@ -0,0 +1,27 @@
+import semver from 'semver';
+
+const defaultConfig = {
+    saucectlVersion: 'latest',
+    workingDirectory: ".",
+    configurationFile: ".sauce/config.yml",
+    region: undefined,
+    suite: undefined,
+};
+
+const get = function() {
+    let sauceConfig = {
+        saucectlVersion: core.getInput('saucectl-version') || defaultConfig.saucectlVersion,
+        workingDirectory: core.getInput('working-directory') || defaultConfig.workingDirectory,
+        configurationFile: core.getInput('working-directory') || defaultConfig.configurationFile,
+        suite: core.getInput('suite') || defaultConfig.suite,
+    };
+
+    if (sauceConfig.saucectlVersion != "latest") {
+        if (!semver.valid(sauceConfig.saucectlVersion)) {
+            core.setFailed(`saucectl-version: ${sauceConfig}: invalid version format`);
+        }
+    }
+    return sauceConfig;
+}
+
+export default { get }
