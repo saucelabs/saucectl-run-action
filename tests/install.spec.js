89a2a80ca389fb6fc487d7ddb6e49e2e4cce2472
Félix P <felix@saucelabs.com>
2021-01-28 12:28:42 -0800

    Add testing

--- tests/install.spec.js
+++ tests/install.spec.js
@@ -5,30 +5,38 @@ const rewire = require("rewire");
 const fs = require("fs");
 
 const nock = require('nock');
-const install = rewire("../src/install.js");
+const install = require("../src/install.js");
 
 beforeAll(() => {
     process.env.GITHUB_ACTION = "FAKE_IN_ACTION";
 });
 
-describe("Version Matching", () => {
-    beforeAll(() => {
-        os.platform.mockImplementation(() => "windows" );
-    });
+it("Version matching", async () => {
+    // rewire wrapping
+    const selectCompatibleVersion = install.selectCompatibleVersion;
+    const testCases = [["0.25.1", "v0.25.1"], ["0.25.0", "v0.25.0"], ["^0.25.0", "v0.25.1"], [">=0.24.0", "v0.25.1"], ["0.24.x", "v0.24.1"]];
+    for (let i = 0; i < testCases.length; i++) {
+        const scope = nock('https://api.github.com')
+            .get('/repos/saucelabs/saucectl/releases')
+            .reply(200, JSON.parse(fs.readFileSync("./tests/fixtures/github-releases-mock.json")));
 
-    it("Version matching", async () => {
-        // rewire wrapping
-        const selectCompatibleVersion = install.__get__("selectCompatibleVersion");
-        testsCases = [["0.25.1", "v0.25.1"], ["0.25.0", "v0.25.0"], ["^0.25.0", "v0.25.1"], [">=0.24.0", "v0.25.1"], ["0.24.x", "v0.24.1"]];
-        for (let i = 0; i < testsCases.length; i++) {
-            const scope = nock('https://api.github.com')
-                .get('/repos/saucelabs/saucectl/releases')
-                .reply(200, JSON.parse(fs.readFileSync("./tests/fixtures/github-releases-mock.json")));
+        let [input, expected] = testCases[i];
+        let version = await selectCompatibleVersion(input);
+        expect(version.tag_name).toBe(expected);
+        expect(scope.isDone()).toBe(true);
+    }
+});
 
-            let [input, expected] = testsCases[i];
-            let version = await selectCompatibleVersion(input);
-            expect(version.tag_name).toBe(expected);
-            expect(scope.isDone()).toBe(true);
-        }
-    });
+it("Platform matching", async () => {
+    const testCases = [
+        ["win32", "x32", "win_32-bit"],
+        ["linux", "x64", "linux_64-bit"],
+        ["darwin", "x64", "mac_64-bit"]
+    ];
+    for (let i = 0; i < testCases.length; i++) {
+        const [osName, archName, expected] = testCases[i];
+        os.platform.mockImplementation(() => osName );
+        os.arch.mockImplementation(() => archName );
+        expect(install.getPlatform()).toBe(expected);
+    }
 });
