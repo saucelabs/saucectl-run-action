78ce0a283aeb691201ea92653bb11597cfa4af00
Félix P <felix@saucelabs.com>
2021-03-09 15:24:16 -0800

    Update envVars

--- src/run.js
+++ src/run.js
@@ -38,7 +38,7 @@ async function saucectlRun(opts) {
     core.info("Launching saucectl !");
     const saucectlArgs = buildSaucectlArgs(opts);
 
-    const child = childProcess.spawn('saucectl', saucectlArgs, { env: { ...process.env, SAUCE_USERNAME: opts.sauceUsername }});
+    const child = childProcess.spawn('saucectl', saucectlArgs, { env: { ...process.env, SAUCE_USERNAME: opts.sauceUsername, SAUCE_ACCESS_KEY: opts.sauceAccessKey }});
     const exitCode = await awaitExecution(child);
 
     if (exitCode != 0) {
