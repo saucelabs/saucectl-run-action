f401339df4c4b84945783f16b45fd545ac52a2eb
Félix P <felix@saucelabs.com>
2023-02-17 18:12:03 +0100

    Avoid installing pre-release by default (#25)
    * Verify this is a stable version

--- tests/install.spec.js
+++ tests/install.spec.js
@@ -9,28 +9,42 @@ const core = require("@actions/core");
 jest.mock("@actions/tool-cache");
 const tc = require("@actions/tool-cache");
 
-
-const nock = require('nock');
+jest.mock('@octokit/rest')
+const { Octokit } = require("@octokit/rest");
 
 const install = require("../src/install.js");
+
+const requestMock = jest.fn();
+requestMock.mockImplementation((target) => new Promise((resolve, reject) => {
+    if (target === "GET /repos/:org/:repo/releases") {
+        resolve({
+            status: 200,
+            headers: { location: 'mock-url' },
+            data: JSON.parse(fs.readFileSync("./tests/fixtures/github-releases-mock.json")),
+        });
+    }
+    reject('Unexpected Endpoint');
+}));
+Octokit.mockImplementation(() => ({ request: requestMock }))
 
 beforeAll(() => {
     process.env.GITHUB_ACTION = "FAKE_IN_ACTION";
 });
 
+beforeEach(() => {
+    jest.clearAllMocks();
+});
+
 it("Version matching", async () => {
     // rewire wrapping
     const selectCompatibleVersion = install.selectCompatibleVersion;
-    const testCases = [["latest", "v0.25.1"], ["0.25.1", "v0.25.1"], ["0.25.0", "v0.25.0"], ["^0.25.0", "v0.25.1"], [">=0.24.0", "v0.25.1"], ["0.24.x", "v0.24.1"]];
+    const testCases = [["latest", "v0.25.1"], ["0.25.1", "v0.25.1"], ["0.25.0", "v0.25.0"], ["^0.25.0", "v0.25.1"], [">=0.24.0", "v0.25.1"], ["0.24.x", "v0.24.1"], ["v0.26.0-alpha1", "v0.26.0-alpha1"]];
     for (let i = 0; i < testCases.length; i++) {
-        const scope = nock('https://api.github.com')
-            .get('/repos/saucelabs/saucectl/releases')
-            .reply(200, JSON.parse(fs.readFileSync("./tests/fixtures/github-releases-mock.json")));
 
         let [input, expected] = testCases[i];
         let version = await selectCompatibleVersion(input);
         expect(version.tag_name).toBe(expected);
-        expect(scope.isDone()).toBe(true);
+        expect(requestMock).toHaveBeenCalled()
     }
 });
 
@@ -52,13 +66,10 @@ describe("Installation", () => {
     it("Invalid version", async () => {
         os.platform.mockReturnValue('linux');
         os.arch.mockReturnValue('x64');
-        const scope = nock('https://api.github.com')
-            .get('/repos/saucelabs/saucectl/releases')
-            .reply(200, JSON.parse(fs.readFileSync("./tests/fixtures/github-releases-mock.json")));
 
         const ret = await install.saucectlInstall({ versionSpec: '0.29.5' });
         expect(ret).toBe(false);
-        expect(scope.isDone()).toBe(true);
+        expect(requestMock).toHaveBeenCalled();
     });
 
     it("Install version - linux", async () => {
@@ -68,14 +79,10 @@ describe("Installation", () => {
         const extractFn = tc.extractTar.mockReturnValue('/tmp/install-dir-extracted');
         const addPathFn = core.addPath.mockReturnValue('/bin/saucectl');
 
-        const scope = nock('https://api.github.com')
-            .get('/repos/saucelabs/saucectl/releases')
-            .reply(200, JSON.parse(fs.readFileSync("./tests/fixtures/github-releases-mock.json")));
-
         const ret = await install.saucectlInstall({ versionSpec: '0.25.1' });
 
         expect(ret).toBe(true);
-        expect(scope.isDone()).toBe(true);
+        expect(requestMock).toHaveBeenCalled();
         expect(downloadFn).toHaveBeenCalled();
         expect(extractFn).toHaveBeenCalled();
         expect(addPathFn).toHaveBeenCalled();
@@ -88,14 +95,10 @@ describe("Installation", () => {
         const extractFn = tc.extractZip.mockReturnValue('/tmp/install-dir-extracted');
         const addPathFn = core.addPath.mockReturnValue('/bin/saucectl');
 
-        const scope = nock('https://api.github.com')
-            .get('/repos/saucelabs/saucectl/releases')
-            .reply(200, JSON.parse(fs.readFileSync("./tests/fixtures/github-releases-mock.json")));
-
         const ret = await install.saucectlInstall({ versionSpec: '0.25.1' });
 
         expect(ret).toBe(true);
-        expect(scope.isDone()).toBe(true);
+        expect(requestMock).toHaveBeenCalled();
         expect(downloadFn).toHaveBeenCalled();
         expect(extractFn).toHaveBeenCalled();
         expect(addPathFn).toHaveBeenCalled();
